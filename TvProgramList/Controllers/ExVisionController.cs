35e08b57d749ce2be0010960eb1efdfb9e9e484b
LightEach <lighteach@gmail.com>
2019-07-08 00:29:26 +0900

    ExVision Map 관련 UI 템플릿 및 구글 맵 구현 추가

---
+++ TvProgramList/Controllers/ExVisionController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TvProgramList.Controllers
+{
+    public class ExVisionController : Controller
+    {
+        // GET: ExVision
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Map()
+        {
+            return View();
+        }
+    }
+}
